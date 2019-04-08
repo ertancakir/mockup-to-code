1dcfe2900450596aee433e8161792831d9bcf067
Ertan Çakır <ertancakiir@gmail.com>
2019-04-08 23:56:42 +0300

    Html kodu oluşturulurken yan yana ve alt alta olan taglar arası boşluk koymayı sağlayan kod eklendi

--- src/code_creator.py
+++ src/code_creator.py
@@ -15,32 +15,69 @@ class CodeCreator(object):
         types = self.result[:,-1]
         comp_rect = self.locations[0]
         same_line_tags = []
-        same_line_tags.append([self.locations[0,0], types[0]])
-        br_height = 0
-        for i in range(1, len(self.result)):
+        
+        same_line_tags.append([comp_rect[0], 0 ,types[0]]) #[x, tag_indis,type]
+        all_tags = []
+
+        for i in range(1, len(self.result)- 1):
             #Taglar yanyana mı?
             if(comp_rect[3] > self.locations[i,1]):
-                same_line_tags.append([self.locations[i,0], types[i]])
+                widht = self.locations[i,2] - self.locations[i,0]
+                heigth = self.locations[i,3] - self.locations[i,1]
+                same_line_tags.append([self.locations[i,0], i ,types[i]])
                 continue
-            else:
-                br_height = self.locations[i,1] - comp_rect[3]
-                #Alt satırdaki yanında olmayan tagı diğerleri ile karşılaştırmak için seç
-                comp_rect = self.locations[i]
-
-                same_line_tags = np.array(same_line_tags)
-                same_line_tags = same_line_tags[same_line_tags[:,0].argsort()]
-                same_line_tags = same_line_tags[::-1]
-                
-                output += self.get_item_code(same_line_tags[:,-1], br_height)
-                same_line_tags = []
-                same_line_tags.append([self.locations[i,0], types[i]])
-                continue
+            
+            #Alt satırdaki yanında olmayan tagı diğerleri ile karşılaştırmak için seç
+            comp_rect = self.locations[i]
 
             same_line_tags = np.array(same_line_tags)
             same_line_tags = same_line_tags[same_line_tags[:,0].argsort()]
             same_line_tags = same_line_tags[::-1]
-            output += self.get_item_code(same_line_tags[:,-1])
+                
+            all_tags.append(same_line_tags)
+            same_line_tags = []
+            same_line_tags.append([self.locations[i,0], i ,types[i]])
+            
+        all_tags.append(same_line_tags)
 
+        tag_x_location = 0
+        tag_y_location = 0
+        all_tags = np.array(all_tags)
+        html = Html()
+
+        for line_tags in all_tags:
+            idx = 0
+            hr_heigth = 0
+            for tags in line_tags:
+                tag_indis = int(tags[1])
+                tag_type = str(tags[2])
+
+                if(idx == 0):
+                    if(tag_y_location != 0):
+                        hr_heigth = self.locations[tag_indis,1] - tag_y_location
+                    tag_y_location = self.locations[tag_indis,3]
+                idx += 1
+                
+                if(tag_x_location != 0):
+                    padding = self.locations[tag_indis,0] - tag_x_location
+                    print padding
+                    output += html.get_padding(padding)
+
+                tag_x_location = self.locations[tag_indis,2]
+                tag_y_location = self.locations[tag_indis,3]
+                widht = self.locations[tag_indis,2] - self.locations[tag_indis,0]
+                heigth = self.locations[tag_indis,3] - self.locations[tag_indis,1]
+                if(tag_type == 'Button'):
+                    output += html.get_button(widht,heigth,"123123")
+                elif(tag_type == 'Text'):
+                    output += html.get_textbox(widht,heigth)
+                elif(tag_type == 'Line'):
+                    output += html.get_text("asdasdasd")
+                elif(tag_type == 'Image'):
+                    output += html.get_image(widht,heigth)
+            
+            tag_x_location = 0
+            output += html.get_hr(hr_heigth)
         
         html_output = ""
         path = os.path.join("web","header.html")
@@ -70,7 +107,7 @@ class CodeCreator(object):
             elif(t == 'Text'):
                 output += html.get_textbox(100,40)
             elif(t == 'Line'):
-                output += "e32e23"
+                output += html.get_text("asdasdasd")
             elif(t == 'Image'):
                 output += html.get_image(200,200)
         
--- src/html_tags.py
+++ src/html_tags.py
@@ -8,13 +8,25 @@ class Html(object):
         self.html_output = ""
 
     def get_button(self, width, height, text):
-        button = "<button class='btn btn-info' style='height:{}px;width:{}px'>{}</button>"
+        button = "<button class='btn-sm btn btn-info' style='height:{}px;width:{}px'>{}</button>"
         return button.format(height, width, text)
 
     def get_image(self, width, height):
         image = "<img src='image.png' width={} height={}>"
         return image.format(width, height)
 
+    def get_text(self,text):
+        span = "<span>{}</span>"
+        return span.format(text)
+
     def get_textbox(self, width, height):
-        text = "<input type='text' class='form-control' style='height:{}px;width:{}px'>"
-        return text.format(height, width)
+        text_box = "<input type='text' class='form-control' style='height:{}px;width:{}px'>"
+        return text_box.format(height, width)
+
+    def get_padding(self, width):
+        span = "<span style='padding-left:{}px'></span>"
+        return span.format(width)
+
+    def get_hr(self, height):
+        hr = "<hr style='height:{}px; visibility:hidden;' />"
+        return hr.format(height)
